79e1cf9fc3c0684edc46334e731787f8e1c6e068
Ken <kennethjay0022@gmail.com>
2024-09-26 23:28:30 +0800

    changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { users } from "./data.js"
 
 const content = document.getElementById("content")
 const main = document.getElementById("container")
-
+let score = ""
 setTimeout(loadingScreen,0)
 
 function loadingScreen(){
@@ -16,11 +16,22 @@ function render(){
         content.innerHTML = 
         `<div class ="container1">
         <h3>Hi ${user.name},</h3>
-        <p>You have <span>${user.tests[0]} pending test</span> this week</p>
-        <div>
-        
+        <p>You have <span class"user-tests">${user.tests[0]} pending test</span> this week</p>
         </div>
-        </div>`
+
+        <div class="container2">
+            <div class="container-21">
+            <h2>Your Grade is <span class"points"></span></h2>
+            <p>Calculate your grade by this simple app</p>
+            </div>
+            <div class="container-22">
+            <button>Calculate Your Grade</button>
+            </div>
+        </div>
+        
+
+        <h3>You have ${user.grades.length} Subjects</h3>
+      `
     })
 
 }
